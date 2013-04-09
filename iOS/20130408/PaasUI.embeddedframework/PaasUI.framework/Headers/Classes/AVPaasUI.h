3d11f2f205fb5903e2972209717b5bedd3ee3573
zeng <zeng.zhu@gmail.com>
2013-04-09 12:11:34 +0800

    update ui framework.

---
+++ iOS/20130408/PaasUI.embeddedframework/PaasUI.framework/Headers/Classes/AVPaasUI.h
@@ -0,0 +1,27 @@
+//
+//  AVPaasUI.h
+//  paas
+//
+//  Created by Summer on 13-3-29.
+//  Copyright (c) 2013年 AVOS. All rights reserved.
+//
+
+#import "AVPaas.h"
+
+#import "AVTableViewCell.h"
+#import "AVQueryTableViewController.h"
+#import "AVImageView.h"
+#import "UIView+AVFields.h"
+#import "AVLogInView.h"
+#import "AVTextField.h"
+#import "AVSignUpView.h"
+#import "AVLogInViewController.h"
+#import "AVSignUpViewController.h"
+
+typedef AVTableViewCell PFTableViewCell;
+typedef AVQueryTableViewController PFQueryTableViewController;
+typedef AVImageView PFImageView;
+typedef AVLogInView PFLoginView;
+typedef AVSignUpView PFSignUpView;
+typedef AVLogInViewController PFLogInViewController;
+typedef AVSignUpViewController PFSignUpViewController;
