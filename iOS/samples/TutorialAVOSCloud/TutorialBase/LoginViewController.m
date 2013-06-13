1a8e2ef504cbba48882da233cd97e86fb5875d08
zengzhu <zeng.zhu@gmail.com>
2013-06-13 15:36:21 +0800

    update framework.

--- iOS/samples/TutorialAVOSCloud/TutorialBase/LoginViewController.m
+++ iOS/samples/TutorialAVOSCloud/TutorialBase/LoginViewController.m
@@ -60,7 +60,8 @@
     [AVUser logInWithUsernameInBackground:self.userTextField.text password:self.passwordTextField.text block:^(AVUser *user, NSError *error) {
         if (user) {
             //Open the wall
-             [self performSegueWithIdentifier:@"LoginSuccesful" sender:self];
+            [self performSegueWithIdentifier:@"avlogin" sender:self];
+//             [self performSegueWithIdentifier:@"LoginSuccesful" sender:self];
         } else {
             //Something bad has ocurred
             NSString *errorString = [[error userInfo] objectForKey:@"error"];
